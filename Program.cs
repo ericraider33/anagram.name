4f7cd089ddb4d72414ff7a0c071773543c037a26
ericraider33 <9611418+ericraider33@users.noreply.github.com>
2021-04-24 19:53:56 -0400

    Blank project structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace anagram.name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
